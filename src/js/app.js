bf131776b59e79208709841f737ad56fb15527f6
oksanterova <oksana.kanterova@gmail.com>
2019-02-12 19:47:41 +0100

    Improve custom widget

--- src/js/app.js
+++ src/js/app.js
@@ -10,43 +10,33 @@ import {
 $(function () {
   $.widget("custom.customize", {
     options: {
-      url1: "https://images.unsplash.com/photo-1501023453312-ce090afb9e40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1085&q=80",
-      url2: "https://images.unsplash.com/photo-1524553496250-1a722745ae00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80",
-      url3: "https://images.unsplash.com/photo-1548700819-892a76eed325?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
-      url4: "https://images.unsplash.com/photo-1532958808832-aae08f887875?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-      url5: "https://images.unsplash.com/photo-1549176755-e67f625da235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1046&q=80",
-      url6: "https://images.unsplash.com/photo-1545383541-325a7082c04e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-      url7: "https://images.unsplash.com/photo-1545577348-1b31905a601c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
-      url8: "https://images.unsplash.com/photo-1508061253366-f7da158b6d46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-      url9: "https://images.unsplash.com/photo-1476837579993-f1d3948f17c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-      url10: "https://images.unsplash.com/photo-1487587090589-f458461e549d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+      urls: [
+        "https://images.unsplash.com/photo-1501023453312-ce090afb9e40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1085&q=80",
+        "https://images.unsplash.com/photo-1524553496250-1a722745ae00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80",
+        "https://images.unsplash.com/photo-1548700819-892a76eed325?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80",
+        "https://images.unsplash.com/photo-1532958808832-aae08f887875?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1549176755-e67f625da235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1046&q=80",
+        "https://images.unsplash.com/photo-1545383541-325a7082c04e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1545577348-1b31905a601c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+        "https://images.unsplash.com/photo-1508061253366-f7da158b6d46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1476837579993-f1d3948f17c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1487587090589-f458461e549d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+      ],
+      delay: 1
     },
-
-    
 
     _create: function () {
-      let url = this._selectImage();
-      $("#customize").on("click", function () {
-        $("body").css("background-image", `url(${url})`);
-      });
+      $("#customize").on("click", this._updateImage.bind(this));
     },
-    
 
-    _selectImage: function () {
-      let imageUrl = [
-        this.options.url1,
-        this.options.url2,
-        this.options.url3,
-        this.options.url4,
-        this.options.url5,
-        this.options.url6,
-        this.options.url7,
-        this.options.url8,
-        this.options.url9,
-        this.options.url10
-      ];
-      let random = Math.floor(Math.random() * 10);
-      return imageUrl[random];
+    _updateImage: function () {
+      let random = Math.floor(Math.random() * this.options.urls.length);
+      let url = this.options.urls[random];
+
+      $("body").css({
+        "transition": `background-image ${this.options.delay}s ease-in-out`,
+        "background-image": `url(${url})`
+      });
     }
   });
 });
